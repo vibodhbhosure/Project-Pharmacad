9aa280d0cd51c8381480f80700c40af5ca06a387
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-06-06 23:27:09 +0530

    Version Changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {
-        loader: "akamai",
-        path: "/",
-    },
     experimental: {
         appDir: true,
     },
