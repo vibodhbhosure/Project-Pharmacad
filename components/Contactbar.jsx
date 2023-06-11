c961e6d15f7d7eb067cd2213fe52e795742fb4b0
TejasBarge02 <barge237@gmail.com>
2023-06-11 18:12:36 +0530

    icons and hover effect added

--- components/Contactbar.jsx
+++ components/Contactbar.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaMapMarkerAlt,FaRegEnvelope,FaPhoneAlt,FaFacebookF,FaLinkedinIn,FaYoutube,FaInstagram,FaGooglePlay } from "react-icons/fa";
 
 function Contactbar() {
   return (
     <div className="fixed left-0 top-0 w-full z-10 mt-2 bg-lime-100">
       <div className="flex p-4 text-black space-x-7 justify-end mr-24">
         <ul className='flex space-x-7'>
-            <li>
-                <Link href="">
-                    <span>i</span>
+            <li className='flex space-x-3'>
+                <Link  href="" >
+                    <span >
+                      <Link className='mt-1' href="/"><FaMapMarkerAlt/></Link>
+                    </span>
                     <span>Dadar, Mumbai-14</span>
                 </Link>
             </li>
-            <li>
+            <li className='flex space-x-3'>
             <Link href="">
-                    <span>i</span>
+                    <span><Link className='mt-1' href="/"><FaRegEnvelope/></Link></span>
                     <span>pharmacad.india@gmail.com</span>
                 </Link>
             </li>
-            <li>
+            <li className='flex space-x-3'>
             <Link href="">
-                    <span>i</span>
+                    <span><Link className='mt-1' href="/"><FaPhoneAlt/></Link></span>
                     <span>(+91)9967 163 961</span>
                 </Link>
             </li>
                 
         </ul>
         <ul className='flex space-x-4'>
-            <li>i</li>
-            <li>i</li>
-            <li>i</li>
-            <li>i</li>
-            <li>i</li>
+            <li className='mt-1'><FaFacebookF/></li>
+            <li className='mt-1'><FaLinkedinIn/></li>
+            <li className='mt-1'><FaYoutube/></li>
+            <li className='mt-1'><FaInstagram/></li>
+            <li className='mt-1'><FaGooglePlay/></li>
         </ul>
       </div>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 import Link from "next/link";
 
+
 const Navbar = () => {
   return (
     <div className="fixed left-0 w-full z-10 mt-10">
       <div className="flex p-4 text-black justify-center space-x-52">
         <Link className="p-4" href='/'><h1>logo</h1></Link>
         <ul className="ml-10 hidden sm:flex">
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Home</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Courses</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Certificate Courses</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Abroad Studies</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Youtube</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Testimonials</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Gallery</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>About Us</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 hover:font-semibold hover:underline">
                 <Link href='/'>Contact</Link>
             </li>
         </ul>
