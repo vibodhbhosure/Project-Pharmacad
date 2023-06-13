6a545acd5ac9af1ca868f7f640f5fc8a45a40647
TejasBarge02 <barge237@gmail.com>
2023-06-13 21:46:13 +0530

    logo and responsive added

--- components/Contactbar.jsx
+++ components/Contactbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import "../src/app/globals.css"
 import { FaMapMarkerAlt,FaRegEnvelope,FaPhoneAlt,FaFacebookF,FaLinkedinIn,FaYoutube,FaInstagram,FaGooglePlay } from "react-icons/fa";
 
 function Contactbar() {
   return (
-    <div className="md:fixed md:left-0 md:top-0 md:w-full md:z-10 md:bg-lime-100 lg:fixed lg:left-0 lg:top-0 lg:w-full lg:z-10 lg:bg-lime-100">
+    <div className="contact md:fixed md:left-0 md:top-0 md:w-full md:z-10 lg:fixed lg:left-0 lg:top-0 lg:w-full lg:z-10 lg:bg-lime-100">
       <div className="hidden md:flex md:p-4 md:text-black md:space-x-7 md:justify-center lg:flex lg:p-4 lg:text-black lg:space-x-7 lg:justify-end lg:mr-24">
         <ul className='md:flex md:space-x-2 lg:flex lg:space-x-7'>
             <li className='flex space-x-3'>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,45 +1,95 @@
 import React from "react";
 import Link from "next/link";
-
+import Image from "next/image";
+import svg1 from '../src/logo/Mask group.svg'
+import svg2 from '../src/logo/Rectangle 408.svg'
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="md:mt-14 lg:fixed lg:left-0 lg:w-full lg:z-10 lg:mt-10 lg:shadow-2xl xl:fixed xl:left-0 xl:w-full xl:z-10 xl:mt-10 xl:shadow-2xl">
-      <div className="lg:flex lg:p-4 lg:text-black lg:justify-end lg:mr-1 xl:flex xl:p-4 xl:text-black xl:justify-end xl:mr-24">
-            <ul className="lg:ml-10 lg:flex xl:ml-10 xl:flex">
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Home</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Courses</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Certificate Courses</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Abroad Studies</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Youtube</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Testimonials</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Gallery</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>About Us</Link>
-                </li>
-                <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
-                    <Link href='/'>Contact</Link>
-                </li>
-            </ul>
+    <div className="flex">
+        <div className="mt-5 justify-start ml-4 z-20 w-40 md:mt-10 md:justify-start md:ml-4 md:z-20 md:h-40 md:w-40 lg:mt-10 lg:justify-start lg:ml-4 lg:z-20 lg:h-40 lg:w-40 xl:mt-10 xl:justify-start xl:ml-24 xl:z-20 xl:h-40 xl:w-40">
+            <Image className="absolute z-30 ml-6 mt-4 md:absolute md:z-30 md:ml-6 md:mt-4 lg:absolute lg:z-30 lg:ml-6 lg:mt-4 xl:absolute xl:z-30 xl:ml-6 xl:mt-4"
+                src={svg1}
+                width={80}
+                height={80}    
+            />
+            <Image className="absolute z-20 md:absolute md:z-20 lg:absolute lg:z-20 xl:absolute xl:z-20"
+                src={svg2}
+                width={130}
+                height={130}
+            />
         </div>
-        
-        
+        <div className="md:mt-14 lg:fixed lg:left-0 lg:w-full lg:z-10 lg:mt-10 xl:fixed xl:left-0 xl:w-full xl:z-10 xl:mt-10 ">
+            <div className="absolute right-0 float-right mr-6 mt-5 text-3xl lg:hidden">
+                <Link className='w-10 h-10' href="/" onClick={onToggleMenu(this)}><FaBars/></Link>
+            </div>
+            <div className="hidden lg:flex lg:p-4 lg:text-black lg:justify-end lg:mr-1 lg:shadow-2xl xl:w-full xl:flex xl:p-4 xl:text-black xl:justify-end xl:mr-24 xl:right-3 xl:shadow-2xl">
+                <ul className="lg:ml-10 lg:flex xl:ml-10 xl:flex">
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Courses</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Certificate Courses</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Abroad Studies</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Youtube</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Testimonials</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Gallery</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>About Us</Link>
+                    </li>
+                    <li className="lg:p-4 lg:px-3 xl:p-4 hover:font-semibold hover:underline">
+                        <Link href='/'>Contact</Link>
+                    </li>
+                </ul>
+            </div>
+            {/* mobile responsive */}
+            <div className="absolute flex mt-40 right-0 mr-1 lg:hidden">
+                <ul className="">
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Courses</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Certificate Courses</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Abroad Studies</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Youtube</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Testimonials</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Gallery</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>About Us</Link>
+                    </li>
+                    <li className="p-4 text-2xl hover:font-semibold hover:underline">
+                        <Link href='/'>Contact</Link>
+                    </li>
+                </ul>
+            </div>
       </div>
+    </div>
+    
   )
 }
-
 export default Navbar
