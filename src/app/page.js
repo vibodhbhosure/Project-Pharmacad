4c9ceaed46de51e1fe3e4c4ec8841f5522468718
Adityaoza93 <aditya.oza@vit.edu.in>
2023-06-17 19:04:58 +0530

    vapasnahikarungachange

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Banner from './Component/Banner'
+import Carousel from './Component/Carousel'
 const Page = () => {
   return (
-    <div>Page</div>
+    <>
+    <Banner />
+    <Carousel /></>
   )
 }
 
