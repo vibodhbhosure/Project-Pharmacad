92cd3c1159fda420a6a6241aedf76668188b7998
Mrunal Tamhane <mrunal.tamhane7@gmail.com>
2023-06-18 19:07:21 +0530

    testimonial container done

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -3,28 +3,32 @@ import TestimonialsCard from "./TestimonialsCard"
 import data from "../testimonials_data.js";
 import "../src/app/globals.css";
 
-const TestimonialsContainer= () => {
-  const testimonialDataArray = Object.values(data);
+const TestimonialsContainer = () => {
+    const testimonialDataArray = Object.values(data);
 
-  console.log(testimonialDataArray);
-  return(
-    <div className="xl:w-full   bg-white ">
-    <div className="mx-32 pt-20">
-        <div className="text-black font-inter font-bold text-3xl">Testimonials</div>
-        <div className="text-black font-inter text-lg">━━ Here’s what our students say about us </div>
-        <div className="flex flex-col">
-        {testimonialDataArray.slice(0, 3).map((cardData) => (
-          <div key={cardData.id} className="lg:w-full ">
-            <TestimonialsCard cardData={cardData} />
-          </div>
-        ))}
+    console.log(testimonialDataArray);
+    return (
+        <div className="w-full  bg-white mb-8">
+            <div className="mx-56 pt-20">
+                <div className="text-black font-inter font-bold text-3xl">Testimonials</div>
+                <div className="text-black font-inter text-lg"> ━━ Here’s what our students say about us </div>
+                <div className="flex flex-col">
+                    {testimonialDataArray.slice(0, 3).map((cardData) => (
+                        <div key={cardData.id} className="lg:w-full ">
+                            <TestimonialsCard cardData={cardData} />
+                        </div>
+                    ))}
 
-          </div>
+                </div>
+                <div>
+                <button class="rounded-md px-5 py-2 bg-[#3A47FB] text-white">Read all testimonials</button>
+</div>
+
+            </div>
         </div>
-    </div>
 
-    
-  )
+
+    )
 }
 
 export default TestimonialsContainer;
--- components/TestimonialsCard.jsx
+++ components/TestimonialsCard.jsx
@@ -3,37 +3,47 @@ import Image from "next/image";
 import svg1 from "../public/cardimages/Burhanuddin_Kagalwala.svg";
 import svg2 from "../public/cardimages/Nichiket_Dandekar.svg";
 import svg3 from "../public/cardimages/Archana_Rajadhyax.svg";
- 
+
 const svgMap = {
-  1: svg1,
-  2: svg2,
-  3: svg3,
+    1: svg1,
+    2: svg2,
+    3: svg3,
 };
 
 const TestimonialsCard = ({ cardData }) => {
-  if (!cardData) {
-    return null; // Return null or handle the case when cardData is undefined
-  }
+    if (!cardData) {
+        return null; // Return null or handle the case when cardData is undefined
+    }
 
-  const {id,testimonial, name, rank } = cardData;
-  const svg = svgMap[id];
+    const { id, testimonial, name, rank } = cardData;
+    const svg = svgMap[id];
 
-  return(
-<div className="bg-white border border-gray-300 border-opacity-60 shadow-md rounded-lg h-max-45 text-black flex flex-row">
-            <div className="flex flex-col">
-            <div>{testimonial}</div>
-            <div>{name}</div>
-            <div>{rank}</div>
-            </div>
-            <div className="flex flex-col">
-            <Image
-            className="lg:mx-5 lg:mt-5 lg:w-16 xl:mx-5 xl:mt-5 xl:w-16 sm:mt-5  sm:pt-10  lg:pt-0 xl:pt-0 sm:ml-6 sm:w-28 xs:mt-5 xs:pt-4 xs:ml-6 xs:w-16"
-            src={svg}
-            alt={name}
-          />
+    return (
+        <div className="bg-white border border-gray-300 border-opacity-60 shadow-md rounded-lg h-max-45 text-black flex flex-row m-2 pb-2 ">
+            <div className="flex flex-row items-center">
+
+                <div className="flex flex-col">
+                    <div className="mx-5 my-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                        </svg></div>
+                    <div className="flex flex-col w-full items-end">
+                        <div className="px-6 font-montserrat font-thin">{testimonial}</div>
+                        <div className="px-6 font-semibold">── {name}</div>
+                        <div className="px-6 font-poppins font-bold text-sm">{rank}</div>
+                    </div>
+
+                </div>
+                <div className="flex flex-col items-center content-center mr-4 w-full p-0  h-full rounded-lg">
+                    <Image
+                        className=" rounded-md mt-4 border-gray-600 shadow-lg"
+                        src={svg}
+                        alt={name}
+                    />
+                </div>
             </div>
         </div>
 
-  );
+    );
 }
-  export default TestimonialsCard;
+export default TestimonialsCard;
