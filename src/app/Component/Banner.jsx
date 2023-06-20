c088f27223d5fb311058537e25c3386c12ec9257
Adityaoza93 <aditya.oza@vit.edu.in>
2023-06-20 13:02:41 +0530

    bannerbuttonfold

--- src/app/Component/Banner.jsx
+++ src/app/Component/Banner.jsx
@@ -1,51 +1,57 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-//import a1 from "../../public/a1.svg";
-//import googleplay from "../../public/googleplay.svg";
+import a1 from "../../public/a.webp";
+import googleplay from "../../public/googleplay.svg";
 const Banner = () => {
   return (
     <>
-      <div className="container mx-auto my-10 ">
-        <div className="bg-[#ffff] px-6 lg:px-16 py-10 rounded-xl">
-          <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
+      <div className="container mx-auto mt-10">
+        <div className="bg-[#ffff] px-6 lg:px-16 rounded-xl ">
+          <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-5">
             <div className="flex flex-col gap-5 lg:gap-10">
-              <p className=" text-base sm:text-1xl md:text-1xl lg:text-2xl font-medium">
+              <p className=" text-xs sm:text-xl md:text-xl lg:text-xl font-medium">
                 #1 COACHING CLASSES FOR PHARMACY IN MUMBAI
               </p>
-
-              <p className=" text-xl font-bold sm:text-2xl md:text-4xl lg:text-6xl ">
-                YOUR Dreams. <br className="lg:flex hidden" />
-                OUR Mission. <br className="lg:flex hidden" />
+              <p className=" text-2xl font-extrabold sm:text-4xl md:text-4xl lg:text-5xl">
+                YOUR Dreams.
               </p>
-              <div className='flex flex-row space-x-4'>
+              <p className=" text-2xl font-extrabold sm:text-4xl md:text-4xl lg:text-5xl -mt-4">
+                OUR Mission.
+              </p>
+              <div className="flex flex-row space-x-4">
+                <button className="flex flex-row justify-center items-center py-3 px-3 gap-2 w-25 h-12 border-solid border-2 border-indigo-600 bg-blue-700 shadow-md rounded-md text-white">
+                  Book free demo
+                </button>
 
-                <button class="flex flex-row justify-center items-center py-3 px-3 gap-2 w-25 h-10 border-solid border-2 border-indigo-600 bg-blue-700 shadow-md rounded-md">Book free demo</button>
-
-                <button class="flex flex-row justify-center items-center py-3 px-3 gap-2 w-25 h-10 border-solid border-2 border-indigo-600 bg-white text-blue-600 shadow-md rounded-md">View all courses</button>
-              </div>              
+                <button className="flex flex-row justify-center items-center py-3 px-3 gap-2 w-25 h-12 border-solid border-2 border-indigo-600 bg-white text-blue-600 shadow-md rounded-md ">
+                  View all courses
+                </button>
+              </div>
               <p className="text-sm sm:text-base lg:text-lg text-[#393939]">
                 Pharmacad is also available on the Playstore!
               </p>
-              
-                <Image className="px-3 py-1.5 flex gap-2 items-center rounded-xl" src="/googleplay.svg" width={200} height={90} />
-                
-            </div>
-            <div>      
-                 
-                    <Image className="left-align" src="/a.png" width={750} height={370} />
-                                  
-            </div>
 
-            
+              <Image
+                className="px-3 py-1.5 flex gap-2 items-center rounded-xl"
+                src={googleplay}
+                width={200}
+                height={90}
+              />
+            </div>
+            <div>
+              <Image
+                priority={true}
+                className="left-align"
+                src={a1}
+                width={750}
+                height={370}
+              />
+            </div>
           </div>
         </div>
       </div>
     </>
+  );
+};
 
-
-
-
-  )
-}
-
-export default Banner
+export default Banner;
