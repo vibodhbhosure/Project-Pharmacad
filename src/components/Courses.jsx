092841e9fd805e5fd5a8c36b2245e806adb02235
Mrunal Tamhane <mrunal.tamhane7@gmail.com>
2023-06-22 01:10:02 +0530

    did population and some css changes

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { FaAngleLeft} from "react-icons/fa";
-
+import coursesdata from './coursesdata';
+import CourseCard from './CourseCard'
 const Courses = () => {
+    const courseDataArray = Object.values(coursesdata);
   return (
     <div className='w-full border-2 border-red-500 mb-[109px]'>
         <div className='h-[38px] w-[42px] ml-[131px] mt-[57px] border-2 border-gray-300 bg-white items-center justify-center absolute rounded-sm p-2 cursor-pointer -z-50'>
@@ -18,32 +20,18 @@ const Courses = () => {
 
             </div>
         </div>
-        <div className='mt-[91px] ml-[131px] border-2 border-green-500'>
-            <span className=' font-bold text-3xl'>
+        <div className='mt-[91px] ml-[131px] border-2 border-green-500 '>
+            <span className=' font-bold text-3xl '>
                 Upcoming Batches
             </span>
-            <div className='grid grid-cols-2 grid-flow-row gap-9'>
-                <div className=' mt-7 border-2 border-cyan-500 h-[478px] w-[572px] shadow-xl'>
-                    <div className='flex flex-row mt-7 ml-10'>
-                        <span className=' font-medium text-3xl'>GPAT <span className=' font-bold text-3xl'>2025</span></span>
-                        <span className=' uppercase bg-orange-600 text-white m-auto px-3 py-2 ml-[75px] font-semibold text-xs'>Offline</span>
-                        <span className='uppercase m-auto px-3 py-2 ml-[11px] font-medium text-xs bg-[#E8F0FC]'>Admissions started</span>
-                    </div>
-                    <div className='w-[490px] bg-[#F5F5F5] mt-5'>
-                        <div>
-                            
-                        </div>
-                    </div>
-                </div>
-                <div className=' mt-7 border-2 border-cyan-500 h-[478px] w-[572px] shadow-xl'>
-
-                </div>
-                <div className=' mt-7 border-2 border-cyan-500 h-[478px] w-[572px] shadow-xl'>
-
-                </div>
-                <div className=' mt-7 border-2 border-cyan-500 h-[478px] w-[572px] shadow-xl'>
-
-                </div>
+            <div className='grid grid-cols-2 grid-flow-row gap-9 '>
+            {courseDataArray.slice(0, 4).map((cardData) => (
+            <div key={cardData.id} className="lg:w-full ">
+              <CourseCard cardData={cardData} />
+            </div>
+          ))}
+                
+                
             </div>
         </div>
     </div>
---
+++ src/components/coursesdata.js
@@ -0,0 +1,65 @@
+const coursesdata = [
+    {
+        "id": 1,
+        "title": "GPAT",
+        "mode": "OFFLINE",
+        "year":2025,
+        "status": "ADMISSIONS STARTED",
+        "desc": ["250+ hours of Live Lectures (only weekends)",
+            "800+ Pre recorded videos for revision & self study",
+            "Expert teachers from Industry for different subjects",
+            "Regular Test Series (150+ Tests)",
+            "E Books & Notes available on the Android App",
+            "Classwork MCQs + Practice MCQs Books More than 10,000 MCQs to practice",
+            "Complementary Certificate Course - Advanced Diploma in Pharmacovigilance @50% off",
+            "Regular Interactive & Motivational sessions for performance analysis, college & course selection"],
+        "price": ""
+
+    },
+    {
+        "id": 2,
+        "title": "GPAT",
+        "mode": "ONLINE",
+        "year":2025,
+        "status": "ADMISSIONS STARTED",
+        "desc": ["250+ hours of Live Lectures (only weekends)",
+            "800+ Pre recorded videos for revision & self study",
+            "Expert teachers from Industry for different subjects",
+            "Regular Test Series (150+ Tests)",
+            "E Books & Notes available on the Android App",
+            "Classwork MCQs + Practice MCQs Books More than 10,000 MCQs to practice",
+            "Complementary Certificate Course - Advanced Diploma in Pharmacovigilance @50% off",
+            "Regular Interactive & Motivational sessions for performance analysis, college & course selection"],
+        "price": ""
+
+    },
+    {
+        "id": 3,
+        "title": "GPAT Test Series",
+        "mode": "OFFLINE",
+        "year":"",
+        "status": "",
+        "desc": ["81 IMPORTANT TOPIC TESTS",
+        "30 TOPIC TESTS",
+        "50 UNIT TESTS",
+        "10 FULL SYLLABUS MOCK TESTS",
+        "10 FULL SYLLABUS SUBJECT TESTS"],
+        "price": ""
+
+    },
+    {
+        "id": 4,
+        "title": "GPAT 2024 Booster ",
+        "mode": "ONLINE",
+        "year":"",
+        "status": "",
+        "desc": ["90+ hrs. live lectures",
+        "800+ pre-recorded videos",
+        "10,000+ MCQs & doubt solving lectures",
+        "Certificate Course: Advanced Diploma in Pharmacovigilance @50% OFF"],
+        "price": "Rs. 5000/-"
+
+    },
+]
+
+export default coursesdata;
