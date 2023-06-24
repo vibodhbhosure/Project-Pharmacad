19ca8c00b95f0d960a01ba8587dab76e0f126f65
suyoghavare <suyoghavare@gmail.com>
2023-06-24 15:43:30 +0530

    Contact - Emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,9 @@
+"use client";
+import { useState } from "react";
 import React from "react";
 import Link from "next/link";
+import emailjs from 'emailjs-com';
+
 import {
   FaMapMarkerAlt,
   FaRegEnvelope,
@@ -11,7 +15,43 @@ import {
   FaGooglePlay,
 } from "react-icons/fa";
 
+
+
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [college, setCollege] = useState('');
+  const [course, setCourse] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_vvw57kk',
+        'template_n8q4g1i',
+        e.target,
+        'lgscseDAAyHiDJEbz'
+      )
+      .then((result) => {
+        console.log(result.text);
+        // Optionally, you can show a success message to the user or redirect them to a thank you page
+      })
+      .catch((error) => {
+        console.error(error.text);
+        // Optionally, you can show an error message to the user
+      });
+
+    // Reset the form fields after submission
+    setName('');
+    setEmail('');
+    setMobile('');
+    setCollege('');
+    setCourse('');
+    setMessage('');
+  };
   return (
     <div
       className="container mx-auto max-w-screen-xl px-4 pt-8 pb-16"
@@ -29,101 +69,108 @@ const Contact = () => {
 
           <div className="flex-1 mt-4 md:mx-6 mr-6">
             {/* Contact Form Content */}
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                Name<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-full border-gray-300 border rounded px-3 py-2"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                Email<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full border-gray-300 border rounded px-3 py-2"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="mobile"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                Mobile<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="tel"
-                id="mobile"
-                className="w-full border-gray-300 border rounded px-3 py-2"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="college"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                College Name<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                id="college"
-                className="w-full border-gray-300 border rounded px-3 py-2"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="course"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                Course Selected<span className="text-red-500">*</span>
-              </label>
-              <select
-                id="course"
-                className="w-full border-gray-300 border rounded px-3 py-2"
-                required
-              >
-                <option value="">Select a course</option>
-                <option value="course1">Course 1</option>
-                <option value="course2">Course 2</option>
-                <option value="course3">Course 3</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="message"
-                className="block text-gray-900 font-medium mb-1"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                rows={5}
-                className="w-full border-gray-300 border rounded px-3 py-2"
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="bg-[#3a47fb] text-white py-2 px-4 shadow-xl rounded"
-            >
-              Send Message
-            </button>
-            <div className="mb-12"></div>
-          </div>
+            <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label htmlFor="name" className="block text-gray-900 font-medium mb-1">
+          Name<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-900 font-medium mb-1">
+          Email<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="mobile" className="block text-gray-900 font-medium mb-1">
+          Mobile<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="tel"
+          id="mobile"
+          name="mobile"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="college" className="block text-gray-900 font-medium mb-1">
+          College Name<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          id="college"
+          name="college"
+          value={college}
+          onChange={(e) => setCollege(e.target.value)}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="course" className="block text-gray-900 font-medium mb-1">
+          Course Selected<span className="text-red-500">*</span>
+        </label>
+        <select
+          id="course"
+          name="course"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+          required
+        >
+          <option value="">Select a course</option>
+          <option value="course1">MBA Entrance Exam</option>
+          <option value="course2">M. Pharmacy Entrance Exam</option>
+          <option value="course3">Abroad Counselling</option>
+          <option value="course4">Pharmacovigilance Certificate Course</option>
+          <option value="course5">Intellectual Patent Rights Certificate Course</option>
+          <option value="course6">MCQ Booklets</option>
+          <option value="course7">GPAT Test Series</option>
+          <option value="course8">GPAT Booster Course</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="message" className="block text-gray-900 font-medium mb-1">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          rows={5}
+          className="w-full border-gray-300 border rounded px-3 py-2"
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="bg-[#3a47fb] text-white py-2 px-4 shadow-xl rounded"
+      >
+        Send Message
+      </button>
+      <div className="mb-12"></div>
+    </form>
+    </div>
           <div className=" bg-[#f2fff0] flex-1 -ml-8">
             {/* Map & Address Details Content */}
             <div className=" md:block mt-0">
