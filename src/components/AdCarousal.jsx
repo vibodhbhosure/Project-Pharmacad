2086787ef1b98908451f606d56b86768843cad48
TejasBarge02 <barge237@gmail.com>
2023-06-28 22:52:55 +0530

    carausal made auto scrolling

--- src/components/AdCarousal.jsx
+++ src/components/AdCarousal.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import Card1 from "./CarousalCards/Card1";
 import Card2 from "./CarousalCards/Card2";
@@ -7,12 +7,26 @@ import Card2 from "./CarousalCards/Card2";
 const AdCarousal = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [MaxIndex, setMaxIndex] = useState(1);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === MaxIndex ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currentIndex, 3000, MaxIndex]);
+
   const prevSlide = () => {
     const isFirstSlide = currentIndex == 0;
     const newIndex = isFirstSlide ? MaxIndex : currentIndex - 1;
     // const newIndex  =isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const nextSlide = () => {
     const isLastSlide = currentIndex === MaxIndex;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
@@ -23,8 +37,9 @@ const AdCarousal = () => {
   return (
     // style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
     <div className="hidden xl:block max-w-[1321px] h-[145px] m-auto mt-28 relative group">
-      <div className="w-full h-full bg-center bg-cover duration-500">
+      <div className="w-full h-full bg-center bg-cover ease-in-out duration-3000">
         {currentIndex === 0 ? <Card1 /> : currentIndex === 1 ? <Card2 /> : ""}
+
       </div>
       <div className="hidden group-hover:flex h-9 w-9 border-2 border-gray-300 bg-white items-center justify-center absolute top-[38%] -translate-x-0 transalate-y-[-50%] left-[20px] rounded-sm p-2 cursor-pointer">
         <FaAngleLeft onClick={prevSlide} />
