98dcf6acae06244f1f55280c198bbc5b87315ad8
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-06-28 14:30:10 +0530

    Navbar full screen

--- src/components/announcements.jsx
+++ src/components/announcements.jsx
@@ -10,7 +10,7 @@ const NationalOlympiad = ({ cardData }) => {
     <div className="flex flex-col font-poppins font-semibold">
       <div>ANNOUNCEMENTS</div>
       <div>
-        <div class="box mx-auto sm:mx-0 w-[310px] sm:w-[600px] md:w-[660px] lg:w-[975px] xl:w-[660px] h-[500px] sm:h-[267px] rounded-lg">
+        <div className="box mx-auto sm:mx-0 w-[310px] sm:w-[600px] md:w-[660px] lg:w-[975px] xl:w-[660px] h-[500px] sm:h-[267px] rounded-lg">
           <div className="mx-12 my-6 py-6">
             <div className="text-3xl font-semibold font-inter w-5/6 text-white">
               {heading}
@@ -24,7 +24,7 @@ const NationalOlympiad = ({ cardData }) => {
               </div>
             </div>
             <Link href="/#contact">
-              <button class="bg-white hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              <button className="bg-white hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                 Register Now
               </button>
             </Link>
