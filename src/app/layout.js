4618e1ad9f0b0f998b855e274d019694018c36d5
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-06-29 19:26:25 +0530

    Sitemap added

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
   description: 'India’s Best Coaching Class for M. Pharmacy, MBA entrance exams & Abroad Studies for Pharmacy students. Experts in GPAT, NIPER, BITS, Manipal, ICT & other entrance exams, also MBA entrance  (CAT / NMAT / CET / SNAP / XAT / CMAT, etc.) exams. More than 30 tutors & content creators from India’s top ranked institutes like NIPER, BITS, ICT, BCP, Punjab University etc., working together to provide you the best & quality education via offline & online platform.',
   charset: 'utf-8',
   viewport: 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no',
+  robots: 'all',
 }
 
 export default function RootLayout({ children }) {
