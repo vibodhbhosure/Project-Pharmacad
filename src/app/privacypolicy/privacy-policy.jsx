fe1d41c258fbf4f9dee47fab3f58c66607591b28
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-06-29 18:12:51 +0530

    Aditya, Tejas Overflow and Suyog Policy Updates

--- src/app/privacypolicy/privacy-policy.jsx
+++ src/app/privacypolicy/privacy-policy.jsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useState } from 'react';
-import privacyPolicyData from './privacy-policy.json';
-import Footer from '@/components/footer';
+import { useEffect, useState } from "react";
+import privacyPolicyData from "./privacy-policy.json";
+import Footer from "@/components/footer";
 
 const PrivacyPolicy = () => {
   const [privacyPolicy, setPrivacyPolicy] = useState({});
@@ -13,20 +13,30 @@ const PrivacyPolicy = () => {
   return (
     <div className="">
       <main className="container mx-auto max-w-screen-xl px-4 py-20">
-        <h1 className="text-4xl font-bold mb-8 font-inter">{privacyPolicy.title}</h1>
-        {privacyPolicy.sections && privacyPolicy.sections.map((section, index) => (
-          <div key={index}>
-            <h2 className="text-2xl font-bold mb-4 font-inter">{section.heading}</h2>
-            <div className="prose font-montserrat text-justify">
-              {section.subsections && section.subsections.map((subsection, idx) => (
-                <div key={idx}>
-                  <h3 className="text-light font-normal mb-2 font-montserrat">{subsection.heading}</h3>
-                  <p className="text-light mb-4 font-montserrat">{subsection.content}</p>
-                </div>
-              ))}
+        <h1 className="text-4xl font-bold mb-8 font-inter">
+          {privacyPolicy.title}
+        </h1>
+        {privacyPolicy.sections &&
+          privacyPolicy.sections.map((section, index) => (
+            <div key={index}>
+              <h2 className="text-2xl font-bold mb-4 font-inter">
+                {section.heading}
+              </h2>
+              <div className="prose font-montserrat text-justify">
+                {section.subsections &&
+                  section.subsections.map((subsection, idx) => (
+                    <div key={idx}>
+                      <h3 className="text-light font-normal mb-2 font-montserrat">
+                        {subsection.heading}
+                      </h3>
+                      <p className="text-light mb-4 font-montserrat">
+                        {subsection.content}
+                      </p>
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </main>
       <Footer />
     </div>
@@ -34,5 +44,3 @@ const PrivacyPolicy = () => {
 };
 
 export default PrivacyPolicy;
-
-
