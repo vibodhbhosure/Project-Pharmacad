a4e02d5508ebc56a6ef4dd4a4fcb30b9e0cddb96
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-07-27 21:10:56 +0530

    Achievers Added

--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,19 @@ import About from '@/components/about'
 import CoursesOffered from '@/components/coursesoffered'
 import Banner from '../components/banner'
 import BestChoiceContainer from "../components/BestChoice"
-import Carousel from '@/components/Carousel'
+// import Carousel from '@/components/Carousel'
 import TestimonialsContainer from '@/components/Testimonials'
 import AdCarousal from '@/components/AdCarousal'
 import Footer from '@/components/footer'
+import Achieve from '@/components/achievers'
 
 export default function Home() {
   return (
     <main>
       <AdCarousal />
       <Banner />
-      <Carousel />
+      {/* <Carousel /> */}
+      <Achieve />
       {/* <CheckoutForm /> */}
       <CoursesOffered />
       <BestChoiceContainer />
